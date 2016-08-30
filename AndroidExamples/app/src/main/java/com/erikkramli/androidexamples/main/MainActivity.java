1ba002f0fd15964433ddd88318516b0ca242cf85
Erik Kramli <Erik.Kramli@betssongroup.com>
2016-08-30 14:08:50 +0200

    Added rxjava dependency for threading purposes.

--- AndroidExamples/app/src/main/java/com/erikkramli/androidexamples/main/MainActivity.java
+++ AndroidExamples/app/src/main/java/com/erikkramli/androidexamples/main/MainActivity.java
@@ -1,15 +1,19 @@
 package com.erikkramli.androidexamples.main;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.erikkramli.androidexamples.R;
+import com.erikkramli.androidexamples.main.infrastructure.MainComponent;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        MainComponent.IoC.satisfy(this);
+
         setContentView(R.layout.activity_main);
     }
 }
