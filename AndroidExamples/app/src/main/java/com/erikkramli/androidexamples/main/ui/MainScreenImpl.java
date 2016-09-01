42271deedce9c995ed964e3ab457361117a72cb1
Erik Kramli <Erik.Kramli@betssongroup.com>
2016-09-01 18:38:02 +0200

    Integrated MainPresenter with MainScreen abstractions. Add progress bar logic.

--- AndroidExamples/app/src/main/java/com/erikkramli/androidexamples/main/ui/MainScreenImpl.java
+++ AndroidExamples/app/src/main/java/com/erikkramli/androidexamples/main/ui/MainScreenImpl.java
@@ -4,61 +4,44 @@ import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 
 import com.erikkramli.androidexamples.R;
-import com.erikkramli.androidexamples.api.model.StarWarsCharacter;
-import com.erikkramli.androidexamples.main.interactor.AllCharacterInteractor;
-
-import java.util.List;
-import java.util.concurrent.Callable;
+import com.erikkramli.androidexamples.main.presenter.MainPresenter;
+import com.erikkramli.androidexamples.util.ViewUtils;
 
 import javax.inject.Inject;
 
-import rx.Observable;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Action1;
-import rx.schedulers.Schedulers;
-
-public class MainScreenImpl implements MainScreen {
+public class MainScreenImpl implements MainScreen, MainPresenter.Callback {
 
     private final AppCompatActivity activity;
-    private final AllCharacterInteractor interactor;
+    private final MainPresenter presenter;
 
     private MainActivityBinding binding;
 
     @Inject
-    public MainScreenImpl(AppCompatActivity activity, AllCharacterInteractor interactor) {
+    public MainScreenImpl(AppCompatActivity activity, MainPresenter presenter) {
         this.activity = activity;
-        this.interactor = interactor;
+        this.presenter = presenter;
     }
 
     @Override
     public void create() {
         binding = DataBindingUtil.setContentView(activity, R.layout.activity_main);
-        binding.text.setText("Loading Star Wars characters");
 
-        Observable
-                .fromCallable(new Callable<String>() {
-                    @Override
-                    public String call() throws Exception {
-                        List<StarWarsCharacter> characters = interactor.getAllStarWarsCharacter();
-                        StringBuilder sb = new StringBuilder();
-                        for (StarWarsCharacter c : characters) {
-                            sb.append(c.getName()).append('\n');
-                        }
-                        return sb.toString();
-                    }
-                })
-                .subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Action1<String>() {
-                    @Override
-                    public void call(String s) {
-                        binding.text.setText(s);
-                    }
-                }, new Action1<Throwable>() {
-                    @Override
-                    public void call(Throwable throwable) {
-                        binding.text.setText(throwable.getMessage());
-                    }
-                });
+        ViewUtils.gone(binding.text);
+        ViewUtils.visible(binding.progress);
+        presenter.loadStarWarsCharacters(this);
+    }
+
+    @Override
+    public void onSuccess(String msg) {
+        ViewUtils.gone(binding.progress);
+        ViewUtils.visible(binding.text);
+        binding.text.setText(msg);
+    }
+
+    @Override
+    public void onFailure() {
+        ViewUtils.gone(binding.progress);
+        ViewUtils.visible(binding.text);
+        binding.text.setText("Nooooo :(");
     }
 }
